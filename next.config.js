2e84c1cb1e815a7ab878fa35d13c1e9b34bfd4d3
Engineer Jamilu Abubakar Sadiq <78563567+Sorondinki@users.noreply.github.com>
2026-07-31 20:36:42 +0100

    Enable trailingSlash in next.config.js
    Added trailingSlash option to fix 404 on refresh.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true, // Wannan ne zai gyara matsalar 404 wajen Refresh!
   images: {
     unoptimized: true,
   },
